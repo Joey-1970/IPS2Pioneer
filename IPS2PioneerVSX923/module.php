6a65e8d08548f1edd10e86e1666aba097d59bf14
Joey-1970 <paeper@horburg.de>
2020-11-13 21:34:47 +0100

    Update module.php

--- IPS2PioneerVSX923/module.php
+++ IPS2PioneerVSX923/module.php
@@ -1046,7 +1046,7 @@ class IPS2PioneerVSX923 extends IPSModule
 		$RadioStationsString = $this->ReadPropertyString("RadioStations");
 		$RadioStations = json_decode($RadioStationsString);
 		foreach ($RadioStations as $Key => $Value) {
-			$this->SendDebug("SetRadioStationsAssociations", "Key: ".$Key." - $Value: ".$Value, 0);
+			$this->SendDebug("SetRadioStationsAssociations", "Key: ".$Key." - Value: ".$Value, 0);
 			IPS_SetVariableProfileAssociation("IPS2Pioneer.RadioStations_".$this->InstanceID, $Key, $Value, "Speaker", -1);
 		}
 	}
