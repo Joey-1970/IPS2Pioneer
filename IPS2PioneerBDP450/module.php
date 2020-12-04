fcdf8f7ffe51bf129aab6f6d843c02c104d8317f
Joey-1970 <paeper@horburg.de>
2020-12-04 19:13:20 +0100

    Update module.php

--- IPS2PioneerBDP450/module.php
+++ IPS2PioneerBDP450/module.php
@@ -608,7 +608,7 @@ class IPS2PioneerBDP450 extends IPSModule
 				$Second = intval(substr($Message, 4, 2));
 				//$Time = date('H:i:s', mktime($Hour, $Minute, $Second, 0, 0, 0));
 				$Time = mktime($Hour, $Minute, $Second, 0, 0, 0);
-				If ($this->SetValue("Time") <> $Time) {
+				If ($this->GetValue("Time") <> $Time) {
 					$this->SetValue("Time", $Time);
 					$this->SetHTMLDisplay($Hour.":".str_pad($Minute, 2, "0", STR_PAD_LEFT).":".str_pad($Second, 2, "0", STR_PAD_LEFT));
 				} 
